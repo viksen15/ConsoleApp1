207c805e14c28d6971d217707fb3916cb7164012
viksen15 <viksen15@users.noreply.github.com>
2020-12-22 16:24:18 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecucion hasta presionar tecla
+        }
+    }
+}
